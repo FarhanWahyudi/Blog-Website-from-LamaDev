fea72cba82d99a95ba1a308db24dcf7cc364b939
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-02-13 20:42:13 +0700

    feat: featured and category list

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,58 @@
+import Link from 'next/link';
 import styles from './categoryList.module.css';
+import Image from 'next/image';
+
+const categories = [
+  {
+    href: '/blog?cat=style',
+    src: '/style.png',
+    class: styles.style,
+    name: 'style',
+  },
+  {
+    href: '/blog?cat=fashion',
+    src: '/fashion.png',
+    class: styles.fashion,
+    name: 'fashion',
+  },
+  {
+    href: '/blog?cat=food',
+    src: '/food.png',
+    class: styles.food,
+    name: 'food',
+  },
+  {
+    href: '/blog?cat=travel',
+    src: '/travel.png',
+    class: styles.travel,
+    name: 'travel',
+  },
+  {
+    href: '/blog?cat=culture',
+    src: '/culture.png',
+    class: styles.culture,
+    name: 'culture',
+  },
+  {
+    href: '/blog?cat=coding',
+    src: '/coding.png',
+    class: styles.coding,
+    name: 'coding',
+  },
+];
 
 export default function CategoryList() {
   return (
-    <div>
-      <h1 className={styles.container}>CategoryList</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <Link href={category.href} className={`${styles.category} ${category.class}`}>
+            <Image src={category.src} alt={category.name} width={32} height={32} className={styles.img} />
+            {category.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import styles from './featured.module.css';
 
 export default function Featured() {
   return (
-    <div>
-      <h1 className={styles.container}>Featured</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Hans here</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} src="/p1.jpeg" alt="p1" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, fugiat.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae doloribus sequi, ratione veniam architecto similique animi a deleniti, sed ipsa qui consequatur tenetur in! Laudantium neque consequuntur doloribus dolorem. Hic
+            cupiditate minima quos possimus fuga voluptatem debitis, doloribus, doloremque itaque repellendus fugiat praesentium? Similique placeat distinctio dolores officia odio.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
   );
 }
