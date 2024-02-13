21feba6d1a897515c779a146c4a9687fbf808370
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-02-13 10:59:18 +0700

    feat: responsive navbar and dark mode

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,17 +1,47 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './authLinks.module.css';
+import { useState } from 'react';
 
 export default function AuthLinks() {
   const status = 'notauthenticated';
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {status === 'notauthenticated' ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/">Write</Link>
-          <Link href="/">Logout</Link>
+          <Link href="/" className={styles.link}>
+            Write
+          </Link>
+          <Link href="/" className={styles.link}>
+            Logout
+          </Link>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+        <span className={styles.line} />
+        <span className={styles.line} />
+        <span className={styles.line} />
+      </div>
+      {isOpen && (
+        <div className={styles.menu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+          {status === 'notauthenticated' ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/">Write</Link>
+              <Link href="/">Logout</Link>
+            </>
+          )}
+        </div>
       )}
     </>
   );
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,12 +1,18 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './themeToggle.module.css';
+import { useContext } from 'react';
+import { ThemeContext } from '@/context/ThemeContext';
 
 export default function ThemeToggle() {
+  const { theme, toggle } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>
-      <Image src="/moon.png" height={14} width={14} />
-      <span className={styles.ball} />
-      <Image src="/sun.png" height={14} width={14} />
+    <div onClick={toggle} className={styles.container} style={theme === 'dark' ? { backgroundColor: '#eee' } : { backgroundColor: '#0f172a' }}>
+      <Image src="/moon.png" alt="moon" height={14} width={14} />
+      <span className={styles.ball} style={theme === 'dark' ? { left: '3px', backgroundColor: '#0f172a' } : { right: '3px', backgroundColor: 'white' }} />
+      <Image src="/sun.png" alt="sun" height={14} width={14} />
     </div>
   );
 }
